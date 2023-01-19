949633a9cc5a78b7ab297ef64bfafb586f7e80e3
Pablofu9 <pablofuertesruiz@gmail.com>
2023-01-19 12:42:17 +0100

    Habla

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pyttsx3
+
+#Inicializamos el asistente
+
 s = pyttsx3.init()
 data = "Hola que tal "
+
+#Aqui seteamos la velocidad del asistente
+rate = s.getProperty('rate')
+s.setProperty('rate', 120)
 s.say(data)
 s.runAndWait()
+
